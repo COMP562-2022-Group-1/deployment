513c41b096e7834a8bd67c65b14598d7f3654f07
Wen Junhua <wenjh1998@gmail.com>
2022-04-21 12:04:50 -0400

    feat: partial completion of frontend

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/submission", methods=["POST"])
+def submission():
+    results = request.form
+    ## Todo: Use the model and save it
+    print(results)
+    result = "The model is still training, please come back in the future when it is completed."
+
+    return render_template("result.html", result=result)
+
+
+@app.route("/assets/<path:path>")
+def send_js(path):
+    return send_from_directory("assets", path)
+
+
+if __name__ == "__main__":
     app.run()
